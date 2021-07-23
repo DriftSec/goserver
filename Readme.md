30ce2aac0be565220d88591b83ebd64bed5dcf0d
Bradley <bradley@shorebreaksecurity.com>
2021-07-23 18:37:38 -0400

    added to readme

--- Readme.md
+++ Readme.md
@@ -1,22 +1,46 @@
+
+
+## GoServer
+### Alternative to python SimpleHTTPServer for using in red teaming.
+ - Dump the full request to view cookies, headers and POST data.
+ - Supports File uploads for data exfiltration
+ - Supports Basic Auth
+ - Supports SSL
+
+### Usage
 ```
 Usage of goserver:
   -addr string
     	Listen address
+  -auth string
+    	Enable basic auth (-auth user:password)
+  -cert string
+    	Path to SSL .crt file
   -dir string
     	Working directory (default "./")
   -dump
     	dump full requests
+  -key string
+    	Path to SSL .key file
   -port string
     	Listen port (default "8000")
+  -ssl
+    	Enable TLS/SSL, requires -key and -cert
 ```
-### Example
-```
-go run main.go -addr 127.0.0.1 -port 3001 -dir /tmp -dump
-```
+##### Example
 
-### uploads:
-    /loot accepts uploads 
-```
-curl -F 'file=@./test.txt' http://127.0.0.1:3001/loot
-```
+Listen on 127.0.0.1:3001, with a working directory of /tmp, dump raw requests, and require auth.
 
+```
+goserver -addr 127.0.0.1 -port 3001 -dir /tmp -dump -auth user:passwerd
+```
+![Alt text](./misc/img.png?raw=true "Example")
+
+### Uploads
+/loot accepts multipart form file uploads. Files are stored in ./ unless -dir is set.
+
+##### Example
+```
+curl -F 'file=@/tmp/test.txt' http://user:passwerd@127.0.0.1:3001/loot
+```
+![Alt text](./misc/im-upload.png?raw=true "Upload")
--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"strings"
 )
 
 const (
@@ -23,14 +26,40 @@ const (
 
 var Dump bool
 var WorkDir string
+var DoAuth bool
+var Username string
+var Password string
+
+func CheckAuth(r *http.Request) bool {
+	if !DoAuth {
+		return true
+	}
+
+	u, p, ok := r.BasicAuth()
+	if !ok {
+		return false
+	}
+	if u != Username {
+		return false
+	}
+	if p != Password {
+		return false
+	}
+	return true
+}
 
 func DropLoot(w http.ResponseWriter, r *http.Request) {
-	DumpReq(w, r)
+	auth := CheckAuth(r)
+	DumpReq(w, r, auth)
+	if !auth {
+		return
+	}
+
 	r.ParseMultipartForm(10 << 20)
 
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		fmt.Println(Red, "[ERROR] Retrieving the File", Reset)
+		fmt.Println(Red+"[ERROR] Retrieving the File", Reset)
 		fmt.Println(err)
 		return
 	}
@@ -43,14 +72,23 @@ func DropLoot(w http.ResponseWriter, r *http.Request) {
 
 	err = ioutil.WriteFile(WorkDir+"/"+handler.Filename, fileBytes, 0644)
 	if err != nil {
-		fmt.Println(Red, "[ERROR] Failed to write local file:", WorkDir+"/"+handler.Filename, Reset)
+		fmt.Println(Red+"[ERROR] Failed to write local file:", WorkDir+"/"+handler.Filename, Reset)
 		return
 	}
-	fmt.Println(Green, "[+] Uploaded File:", handler.Filename, Reset)
+	fmt.Println(Green+"[+] Uploaded File:", handler.Filename, Reset)
 }
 
-func DumpReq(w http.ResponseWriter, req *http.Request) {
-	log.Println(req.Method, "from", req.RemoteAddr+":", req.URL.Path)
+func DumpReq(w http.ResponseWriter, req *http.Request, auth bool) {
+	authmsg := ""
+	if DoAuth {
+		if auth {
+			authmsg = Green + "(Auth Valid)" + Reset
+		} else {
+			authmsg = Red + "(Auth Failed)" + Reset
+		}
+
+	}
+	log.Println(req.Method, "from", req.RemoteAddr+":", req.URL.Path, authmsg)
 	if Dump {
 		qp := ""
 		if len(req.URL.RawQuery) > 0 {
@@ -62,14 +100,14 @@ func DumpReq(w http.ResponseWriter, req *http.Request) {
 		body, err := ioutil.ReadAll(req.Body)
 
 		if err != nil {
-			log.Printf(Red, "[ERROR] reading body: %v", err, Reset)
+			log.Println(Red+"[ERROR] reading body:", err, Reset)
 			return
 		}
 
-		fmt.Println()
-		fmt.Println()
+		// fmt.Println()
+		// fmt.Println()
 
-		fmt.Println("-------------------------------------------------------------------------------------")
+		fmt.Println(Cyan + "-------------------------------------- Request --------------------------------------")
 
 		fmt.Println(req.Method + " " + req.URL.Path + qp + " " + req.Proto)
 		fmt.Println("Host: " + req.Host)
@@ -80,16 +118,42 @@ func DumpReq(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(string(body))
 		req.Body.Close() //  must close
 		req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		fmt.Println("-------------------------------------------------------------------------------------", Reset)
+
 	}
 
 }
 
 func ServeFiles(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		DumpReq(w, r)
+		auth := CheckAuth(r)
+		DumpReq(w, r, auth)
+		if !auth {
+			return
+		}
 		h.ServeHTTP(w, r) // call original
 
 	})
+}
+
+func listURLs(addr, port string, ssl bool) {
+	scheme := "http"
+	if ssl {
+		scheme = "https"
+	}
+	fmt.Println(Blue+"[!] Starting web server at:", Reset)
+	if addr != "" {
+		fmt.Println(Yellow+"     "+scheme+"://"+addr+":"+port, Reset)
+	} else {
+		ip, err := net.InterfaceAddrs()
+		if err == nil {
+			for _, v := range ip {
+				fmt.Println(Yellow+"     "+scheme+"://"+strings.Split(v.String(), "/")[0]+":"+port, Reset)
+
+			}
+		}
+	}
+	fmt.Println()
 }
 
 func main() {
@@ -98,14 +162,52 @@ func main() {
 	addr := flag.String("addr", "", "Listen address")
 	dump := flag.Bool("dump", false, "dump full requests")
 	workdir := flag.String("dir", "./", "Working directory")
+	auth := flag.String("auth", "", "Enable basic auth (-auth user:password)")
+	ssl := flag.Bool("ssl", false, "Enable TLS/SSL, requires -key and -cert")
+	sslcrt := flag.String("cert", "", "Path to SSL .crt file")
+	sslkey := flag.String("key", "", "Path to SSL .key file")
+
 	flag.Parse()
 
 	WorkDir = *workdir
 	Dump = *dump
-	fmt.Println(Blue, "[!] Starting web server at:", *addr+":"+*port, Reset)
-	// http.HandleFunc("/dump", DumpReq)
+
+	if *ssl {
+		if *sslcrt == "" || *sslkey == "" {
+			fmt.Println(Red+"[ERROR] -cert and -key are required for SSL.", Reset)
+			os.Exit(1)
+		}
+	}
+
+	if *auth != "" {
+		parts := strings.Split(*auth, ":")
+		if len(parts) != 2 {
+			fmt.Println(Red+"[ERROR] invalid auth string: must be 'user:password'", Reset)
+			os.Exit(1)
+		} else {
+			DoAuth = true
+			Username = parts[0]
+			Password = parts[1]
+		}
+	}
+
+	fmt.Println(Blue+"[!] Dump Requests:", *dump, Reset)
+	fmt.Println(Blue+"[!] Auth Enabled:", DoAuth, Reset)
+	fmt.Println(Blue+"[!] Working Directory:", *workdir, Reset)
+	fmt.Println(Blue+"[!] SSL Enabled:", *ssl, Reset)
+	listURLs(*addr, *port, *ssl)
 	http.HandleFunc("/loot", DropLoot)
-	// http.Handle("/", http.FileServer(http.Dir(*workdir)))
 	http.Handle("/", ServeFiles(http.FileServer(http.Dir(*workdir))))
-	http.ListenAndServe(*addr+":"+*port, nil)
+
+	if *ssl {
+		err := http.ListenAndServeTLS(*addr+":"+*port, *sslcrt, *sslkey, nil)
+		if err != nil {
+			log.Fatal("ListenAndServeTLS: ", err)
+		}
+	} else {
+		err := http.ListenAndServe(*addr+":"+*port, nil)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	}
 }
